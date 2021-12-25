f67a9785dc597ed42618a42223378ced3e199df1
jatinkesharwani <jatinkesharwani360@gmail.com>
2021-12-26 00:42:20 +0530

    refactor: style, shrinkWrap, scrollDirection

--- lib/screens/faqs_page.dart
+++ lib/screens/faqs_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:covid19_tracker/Bloc/navigation_bloc.dart';
 import 'package:covid19_tracker/widgets/FAQs_Data/faqs_data.dart';
 import 'package:flutter/material.dart';
@@ -9,22 +10,54 @@ class FAQPage extends StatelessWidget with NavigationStates {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FAQs'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: const AutoSizeText(
+          "FAQs",
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: "Montserrat",
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+          minFontSize: 14,
+          stepGranularity: 2,
+          maxLines: 1,
+        ),
       ),
       body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
           itemCount: DataSource.questionAnswers.length,
           itemBuilder: (context, index) {
-            return ExpansionTile(
-              title: Text(
-                DataSource.questionAnswers[index]['question'],
-                style: const TextStyle(fontWeight: FontWeight.bold),
+            return Card(
+              child: ExpansionTile(
+                title: Padding(
+                  padding: const EdgeInsets.only(left: 26),
+                  child: Text(
+                    DataSource.questionAnswers[index]['question'],
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(DataSource.questionAnswers[index]['answer'],
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Montserrat",
+                      //color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),),
+                  )
+                ],
               ),
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(DataSource.questionAnswers[index]['answer']),
-                )
-              ],
             );
           }),
     );
